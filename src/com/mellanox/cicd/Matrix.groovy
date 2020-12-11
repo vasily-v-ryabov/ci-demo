4870a38fa57f52267284ae41fc5e70d297cb5b28
Vasily Ryabov <vryabov@nvidia.com>
2020-12-11 17:54:02 +0300

    Call script to prepare Docker build context.

--- src/com/mellanox/cicd/Matrix.groovy
+++ src/com/mellanox/cicd/Matrix.groovy
@@ -663,6 +663,9 @@ def buildDocker(image, config) {
                 need_build++
             }
             if (need_build) {
+                if (image.prepare) {
+                    run_shell(image.prepare, "Preparing Docker build context for ${filename}")
+                }
                 config.logger.info("Building - ${img} - ${filename}")
                 buildImage(img, filename, extra_args, config)
             }
@@ -805,7 +808,7 @@ def main() {
             def arch_distro_map = gen_image_map(config)
             arch_distro_map.each { arch, images ->
                 images.each { image ->
-                    parallelBuildDockers[image.name] = {
+                    parallelBuildDockers[image.name + "." + arch] = {
                         if (image.nodeLabel) {
                             runDocker(image, config, "Preparing docker image", null, { pimage, pconfig -> buildDocker(pimage, pconfig) }, false)
                         } else {
@@ -815,7 +818,7 @@ def main() {
                     branches += getMatrixTasks(image, config)
                 }
             }
-        
+
             try {
                 def bSize = getConfigVal(config, ['batchSize'], 0)
                 def timeout_min = getConfigVal(config, ['timeout_minutes'], "90")
@@ -829,9 +832,12 @@ def main() {
                 if (config.pipeline_stop) {
                     def cmd = config.pipeline_stop.run
                     if (cmd) {
-                        logger.debug("running pipeline_stop")
+                        logger.debug("Running pipeline_stop")
                         stage("Stop ${config.job}") {
-                            run_shell("${cmd}", "stop")
+                            withCredentials([usernamePassword(credentialsId: "${config.registry_auth}",
+                            usernameVariable: 'HARBOR_USER', passwordVariable: 'HARBOR_PASSWORD')]) {
+                                run_shell("${cmd}", "stop")
+                            }
                         }
                     }
                 }
